ef21b53fe8e889595b2149b92afca797633c4092
mavedev <usatchoff@gmail.com>
2020-04-28 21:45:59 +0300

    Field accessor level error for current date fixed

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  private currentDate = new Date();
+  currentDate = new Date();
 
   constructor() { }
 
