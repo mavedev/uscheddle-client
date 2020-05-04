5ab2270bf474cd19b62f16bf854446eba9cf1d82
mavedev <usatchoff@gmail.com>
2020-05-04 22:39:38 +0300

    The schedview page added to the paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SchedlistComponent } from './schedlist/schedlist.component';
+import { SchedviewComponent } from './schedview/schedview.component';
 import { MainRequestPageComponent } from './main-request-page/main-request-page.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'schedlist', component: SchedlistComponent },
+  { path: 'schedview', component: SchedviewComponent },
   { path: 'main-request', component: MainRequestPageComponent }
 ];
 
