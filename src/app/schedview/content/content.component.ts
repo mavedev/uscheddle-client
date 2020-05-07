d3b440fbd5ab5b1bb7d61e15cb267daebcf76a3e
mavedev <usatchoff@gmail.com>
2020-05-07 23:35:04 +0300

    User info loading from Microsoft provided

--- src/app/schedview/content/content.component.ts
+++ src/app/schedview/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MsalService } from '@azure/msal-angular';
 
 @Component({
   selector: 'app-schedview-content',
@@ -11,16 +12,11 @@ export class ShedviewContentComponent implements OnInit {
   private scheduleId: string;
   private userId: string;
 
-  public constructor(private route: ActivatedRoute) { }
+  public constructor(private route: ActivatedRoute, private authService: MsalService) { }
 
   public ngOnInit(): void {
     this.scheduleId = this.route.snapshot.paramMap.get('id');
-    this.userId = this.getUserId();
-    alert([this.scheduleId, this.userId]);
-  }
-
-  private getUserId(): string {
-    return 'dummyUser';
+    this.userId = this.authService.getAccount().accountIdentifier || '';
   }
 
 }
