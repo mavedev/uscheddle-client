16970f545accacc4dd558ae5f0cd60a69f556af9
mavedev <usatchoff@gmail.com>
2020-05-08 10:24:59 +0300

    Course adding to the form provided

--- src/app/main-request-page/content/content.component.ts
+++ src/app/main-request-page/content/content.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { MsalService } from '@azure/msal-angular';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+import { Course } from './course.model';
 
 @Component({
   selector: 'app-main-request-page-content',
@@ -33,6 +34,10 @@ export class MainRequestPageContentComponent implements OnInit {
   public ngOnInit(): void {
   }
 
+  private get coursesArray(): FormArray {
+    return this.coursesFormGroup.get('coursesArray') as FormArray;
+  }
+
   private createCoursesForm(): void {
     this.coursesFormGroup = this.formBuilder.group({
       name: '',
@@ -41,6 +46,10 @@ export class MainRequestPageContentComponent implements OnInit {
       hours: '',
       coursesArray: this.formBuilder.array([])
     });
+  }
+
+  private addCourseToTheForm(): void {
+    this.coursesArray.push(this.formBuilder.group(new Course()));
   }
 
   public sendGenerateRequest(): void {
