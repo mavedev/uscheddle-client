17661a67aa0944895fddb13b6ccc0fbcc1ae6a22
mavedev <usatchoff@gmail.com>
2020-05-08 09:24:35 +0300

    Schedview main warning implemented

--- src/app/schedview/content/content.component.ts
+++ src/app/schedview/content/content.component.ts
@@ -1,6 +1,8 @@
+import { environment } from './../../../environments/environment';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MsalService } from '@azure/msal-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-schedview-content',
@@ -9,17 +11,34 @@ import { MsalService } from '@azure/msal-angular';
 })
 export class ShedviewContentComponent implements OnInit {
 
+  private readonly apiReadURL = `${environment.apiURI}/schedule`;
+
   private scheduleId: string;
   private userId: string;
 
+  public isAbleToLoad = true;
+
   public constructor(
     private route: ActivatedRoute,
+    private httpClient: HttpClient,
     private authService: MsalService
   ) { }
 
   public ngOnInit(): void {
     this.scheduleId = this.route.snapshot.paramMap.get('id');
     this.userId = this.authService.getAccount().accountIdentifier || '';
+    this.getScheduleData();
+  }
+
+  private getScheduleData() {
+    this.httpClient.get(
+      `${this.apiReadURL}/${this.scheduleId}?senderId=${this.userId}`,
+      { headers: { 'Content-Type': 'application/json' } }
+    )
+    .subscribe({
+      next: (_: any) => {  },
+      error: (_) => { this.isAbleToLoad = false; }
+    });
   }
 
 }
