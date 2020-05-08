be6b4fc1e6e8729f96e29c691bcccfcbd7f1dfbb
mavedev <usatchoff@gmail.com>
2020-05-08 10:25:26 +0300

    Default values for the course constructor provided

--- src/app/main-request-page/content/course.model.ts
+++ src/app/main-request-page/content/course.model.ts
@@ -1,10 +1,10 @@
 export class Course {
 
   public constructor(
-    public name: string,
-    public courseType: string,
-    public instructors: string,
-    public hours: number
+    public name: string = '',
+    public courseType: string = '',
+    public instructors: string = '',
+    public hours: number = 0
   ) { }
 
 }
